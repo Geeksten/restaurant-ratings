e27258530a2de96d235818c9a8d2c239058b043f
Hackbright Student <info@hackbrightacademy.com>
2015-09-28 17:30:09 -0700

    Code reviewed.  Original assignment completed. Time for some further study.

---
+++ restaurant-ratings.py
@@ -0,0 +1,19 @@
+def ratings_format(filename):
+    """Takes txt file with list of restaurant ratings and prints strings of restaurant name and rating value.
+    """
+
+    restaurant_ratings_dict = {}
+
+    ratings_file = open(filename)
+    
+    for line in ratings_file:
+        name_score = line.rstrip()
+        name_score = name_score.split(":")
+        restaurant_ratings_dict[name_score[0]] = name_score[1]
+    
+    sorted_ratings_dict = sorted(restaurant_ratings_dict)
+
+    for restaurant in sorted_ratings_dict:
+        print "{} is rated at {}.".format(restaurant, restaurant_ratings_dict[restaurant])
+
+ratings_format("scores.txt")
