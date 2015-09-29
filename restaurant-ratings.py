b7d5e7295aac60eef0a45d91f87198923f3f84e0
Hackbright Student <info@hackbrightacademy.com>
2015-09-28 17:53:38 -0700

    Prompts for a new restaurant/rating before printing list

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -5,11 +5,16 @@ def ratings_format(filename):
     restaurant_ratings_dict = {}
 
     ratings_file = open(filename)
-    
+
     for line in ratings_file:
         name_score = line.rstrip()
         name_score = name_score.split(":")
         restaurant_ratings_dict[name_score[0]] = name_score[1]
+
+    print "Add a new restaurant."
+    new_restaurant = raw_input("Restaurant: ")
+    new_rating = int(raw_input("Rating: "))
+    restaurant_ratings_dict[new_restaurant] = new_rating
     
     sorted_ratings_dict = sorted(restaurant_ratings_dict)
 
